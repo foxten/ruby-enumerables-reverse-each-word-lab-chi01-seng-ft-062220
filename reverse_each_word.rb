306bcbdffd0b88afc7085d507cf2f48e852d60f8
Tenaysia Fox <tenaysiafox@gmail.com>
2020-05-28 17:54:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,7 @@
 def reverse_each_word(string)
- new_string = string.split.collect do |element|
+ #new_string = string.split.collect do |element|
+  #element.reverse
+  new_string = string.split.each do |element|
   element.reverse
 end
 new_string.join(" ")
